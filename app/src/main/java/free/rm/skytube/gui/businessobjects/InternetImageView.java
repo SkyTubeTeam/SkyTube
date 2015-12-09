495c8ac4b5414f8391319558f0d9a0f466b7463e
Ramon M <ramon.mifsud@yandex.com>
2015-12-09 19:39:35 +0100

    This app is licensed under the GNU General Public License version 3 ONLY.

--- app/src/main/java/free/rm/skytube/gui/businessobjects/InternetImageView.java
+++ app/src/main/java/free/rm/skytube/gui/businessobjects/InternetImageView.java
@@ -4,8 +4,7 @@
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * the Free Software Foundation (version 3 of the License).
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -38,8 +37,7 @@ import java.net.URL;
  *
  * <p>This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.</p>
+ * the Free Software Foundation (version 3 of the License).
  *
  * <p>This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
