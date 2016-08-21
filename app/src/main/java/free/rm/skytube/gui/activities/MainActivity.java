c8d4b717c82212c30c9b3ffb975e622515b38d14
Ramon M <ramon.mifsud@yandex.com>
2016-08-21 19:35:25 +0200

    Allows the user to explicitly enter youtube video URL

--- app/src/main/java/free/rm/skytube/gui/activities/MainActivity.java
+++ app/src/main/java/free/rm/skytube/gui/activities/MainActivity.java
@@ -17,14 +17,21 @@
 
 package free.rm.skytube.gui.activities;
 
+import android.content.ClipDescription;
+import android.content.ClipboardManager;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 import free.rm.skytube.R;
 
@@ -82,9 +89,67 @@ public class MainActivity extends AppCompatActivity {
 				Intent i = new Intent(this, PreferencesActivity.class);
 				startActivity(i);
 				return true;
+			case R.id.menu_enter_video_url:
+				displayEnterVideoUrlDialog();
 		}
 
 		return super.onOptionsItemSelected(item);
+	}
+
+
+	/**
+	 * Display the Enter Video URL dialog.
+	 */
+	private void displayEnterVideoUrlDialog() {
+		final AlertDialog alertDialog = new AlertDialog.Builder(this)
+			.setView(R.layout.dialog_enter_video_url)
+			.setTitle(R.string.enter_video_url)
+			.setPositiveButton(R.string.play, new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					// get the inputted URL string
+					final String videoUrl = ((EditText)((AlertDialog) dialog).findViewById(R.id.dialog_url_edittext)).getText().toString();
+
+					// play the video
+					Intent i = new Intent(MainActivity.this, YouTubePlayerActivity.class);
+					i.setAction(Intent.ACTION_VIEW);
+					i.setData(Uri.parse(videoUrl));
+					startActivity(i);
+				}
+			})
+			.setNegativeButton(R.string.cancel, null)
+			.show();
+
+		// paste whatever there is in the clipboard (hopefully it is a video url)
+		((EditText) alertDialog.findViewById(R.id.dialog_url_edittext)).setText(getClipboardItem());
+
+		// clear URL edittext button
+		alertDialog.findViewById(R.id.dialog_url_clear_button).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				((EditText) alertDialog.findViewById(R.id.dialog_url_edittext)).setText("");
+			}
+		});
+	}
+
+
+	/**
+	 * Return the last item stored in the clipboard.
+	 *
+	 * @return	{@link String}
+	 */
+	private String getClipboardItem() {
+		String item = "";
+
+		ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+		if (clipboard.hasPrimaryClip()) {
+			android.content.ClipDescription description = clipboard.getPrimaryClipDescription();
+			android.content.ClipData data = clipboard.getPrimaryClip();
+			if (data != null && description != null && description.hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN))
+				item = String.valueOf(data.getItemAt(0).getText());
+		}
+
+		return item;
 	}
 
 }
