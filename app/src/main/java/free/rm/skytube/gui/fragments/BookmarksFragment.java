SkyTubeTeam/SkyTube

commit c8d27cedcdf0c2eae6dc1fa2822450d562ee8d54

- Code polishing mainly with respect to MainFragment:  i.e. no longer using fixed fragment position, but rather an array of fragments. - Code normalization wrt VideoCategory. - Continued renaming "saved videos" instances to "bookmarked".

app/src/main/java/free/rm/skytube/gui/fragments/BookmarksFragment.java
@@ -27,18 +27,21 @@ import android.view.ViewGroup;
 import butterknife.Bind;
 import free.rm.skytube.R;
 import free.rm.skytube.businessobjects.MainActivityListener;
+import free.rm.skytube.businessobjects.VideoCategory;
 import free.rm.skytube.businessobjects.db.BookmarksDb;
-import free.rm.skytube.gui.businessobjects.SavedVideoGridAdapter;
+import free.rm.skytube.gui.app.SkyTubeApp;
 import free.rm.skytube.gui.businessobjects.SimpleItemTouchHelperCallback;
+import free.rm.skytube.gui.businessobjects.bookmarksGridAdapter;
 
 /**
  * Fragment that displays bookmarked videos.
  */
-public class BookmarksFragment extends VideosGridFragment implements BookmarksDb.SavedVideosDbListener {
+public class BookmarksFragment extends VideosGridFragment implements BookmarksDb.BookmarksDbListener {
 	@Bind(R.id.noSavedVideosText)
 	View noBookmarkedVideosText;
 
-	private SavedVideoGridAdapter savedVideoGridAdapter;
+	private bookmarksGridAdapter bookmarksGridAdapter;
+
 
 	@Override
 	public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -46,22 +49,25 @@ public class BookmarksFragment extends VideosGridFragment implements BookmarksDb
 		setLayoutResource(R.layout.videos_gridview_bookmarks);
 	}
 
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View view = super.onCreateView(inflater, container, savedInstanceState);
-		if(savedVideoGridAdapter == null)
-			savedVideoGridAdapter = new SavedVideoGridAdapter(getActivity());
-		gridView.setAdapter(savedVideoGridAdapter);
 
-		savedVideoGridAdapter.clearList();
+		if (bookmarksGridAdapter == null)
+			bookmarksGridAdapter = new bookmarksGridAdapter(getActivity());
 
-		savedVideoGridAdapter.setListener((MainActivityListener)getActivity());
+		bookmarksGridAdapter.clearList();
+		bookmarksGridAdapter.setListener((MainActivityListener)getActivity());
+		gridView.setAdapter(bookmarksGridAdapter);
 
-		ItemTouchHelper.Callback callback = new SimpleItemTouchHelperCallback(savedVideoGridAdapter);
+		ItemTouchHelper.Callback callback = new SimpleItemTouchHelperCallback(bookmarksGridAdapter);
 		ItemTouchHelper touchHelper = new ItemTouchHelper(callback);
 		touchHelper.attachToRecyclerView(gridView);
+
 		return view;
 	}
+
 
 	@Override
 	public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
@@ -70,34 +76,47 @@ public class BookmarksFragment extends VideosGridFragment implements BookmarksDb
 		populateList();
 	}
 
+
 	private void populateList() {
 		int numVideosSaved = BookmarksDb.getBookmarksDb().getNumVideos();
+
 		// If no videos have been saved, show the text notifying the user, otherwise
 		// show the swipe refresh layout that contains the actual video grid.
-		if (numVideosSaved == 0) {
+		if (numVideosSaved <= 0) {
 			swipeRefreshLayout.setVisibility(View.GONE);
 			noBookmarkedVideosText.setVisibility(View.VISIBLE);
 		} else {
 			swipeRefreshLayout.setVisibility(View.VISIBLE);
 			noBookmarkedVideosText.setVisibility(View.GONE);
 
-			savedVideoGridAdapter.updateList(BookmarksDb.getBookmarksDb().getSavedVideos());
+			bookmarksGridAdapter.setVideoCategory(VideoCategory.BOOKMARKS_VIDEOS);
 		}
 	}
 
-	/**
-	 * When
-	 */
-	public void onSelected() {
-		if(BookmarksDb.getBookmarksDb().isHasUpdated()) {
+
+	@Override
+	protected void onFragmentSelected() {
+		if (BookmarksDb.getBookmarksDb().isHasUpdated()) {
 			populateList();
 			BookmarksDb.getBookmarksDb().setHasUpdated(false);
 		}
 	}
 
+
 	@Override
-	public void onUpdated() {
+	public void onBookmarksDbUpdated() {
 		populateList();
+		bookmarksGridAdapter.refresh();
+	}
+
+	@Override
+	protected VideoCategory getVideoCategory() {
+		return VideoCategory.BOOKMARKS_VIDEOS;
+	}
+
+	@Override
+	protected String getFragmentName() {
+		return SkyTubeApp.getStr(R.string.bookmarks);
 	}
 
 }

app/src/main/java/free/rm/skytube/gui/fragments/SubscriptionsFragment.java
@@ -19,7 +19,6 @@ package free.rm.skytube.gui.fragments;
 
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Looper;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.Toast;
@@ -28,17 +27,19 @@ import com.afollestad.materialdialogs.MaterialDialog;
 
 import butterknife.Bind;
 import free.rm.skytube.R;
-import free.rm.skytube.businessobjects.AsyncTaskParallel;
 import free.rm.skytube.businessobjects.GetSubscriptionVideosTask;
+import free.rm.skytube.businessobjects.VideoCategory;
 import free.rm.skytube.businessobjects.YouTubeChannel;
 import free.rm.skytube.businessobjects.db.SubscriptionsDb;
+import free.rm.skytube.gui.app.SkyTubeApp;
 import free.rm.skytube.gui.businessobjects.SubsAdapter;
 import free.rm.skytube.gui.businessobjects.SubscriptionsFragmentListener;
 
 /**
- * Fragment that displays videos from all channels the user is subscribed to
+ * Fragment that displays videos from all channels the user is subscribed to.
  */
 public class SubscriptionsFragment extends VideosGridFragment implements SubscriptionsFragmentListener {
+
 	private int numVideosFetched = 0;
 	private int numChannelsFetched = 0;
 	private int numChannelsSubscribed = 0;
@@ -61,7 +62,7 @@ public class SubscriptionsFragment extends VideosGridFragment implements Subscri
 		super.onViewCreated(view, savedInstanceState);
 		videoGridAdapter.clearList();
 
-		numChannelsSubscribed = SubscriptionsDb.getSubscriptionsDb().numSubscribedChannels();
+		numChannelsSubscribed = SubscriptionsDb.getSubscriptionsDb().getTotalSubscribedChannels();
 		if(numChannelsSubscribed == 0) {
 			swipeRefreshLayout.setVisibility(View.GONE);
 			noSubscriptionsText.setVisibility(View.VISIBLE);
@@ -71,8 +72,7 @@ public class SubscriptionsFragment extends VideosGridFragment implements Subscri
 				noSubscriptionsText.setVisibility(View.GONE);
 			}
 
-
-			populateList();
+			videoGridAdapter.setVideoCategory(VideoCategory.SUBSCRIPTIONS_FEED_VIDEOS);
 
 			// Launch a refresh of subscribed videos when this Fragment is created, but don't show the progress dialog. It will be shown when the tab is shown.
 			if (shouldRefresh)
@@ -81,27 +81,11 @@ public class SubscriptionsFragment extends VideosGridFragment implements Subscri
 		}
 	}
 
-	private void populateList() {
-		// {@link SubscriptionsDb.getSubscriptionsDb().getSubscriptionVideos()} should not be called in the UI thread here, so as to slow down the UI.
-		new AsyncTaskParallel<Void, Void, Void>() {
-			@Override
-			protected Void doInBackground(Void... voids) {
-				// Modifying any views needs to be done in the UI thread.
-				new Handler(Looper.getMainLooper()).post(new Runnable() {
-					@Override
-					public void run() {
-						videoGridAdapter.appendList(SubscriptionsDb.getSubscriptionsDb().getSubscriptionVideos());
-					}
-				});
-				return null;
-			}
-		}.executeInParallel();
-	}
 
 	private void doRefresh(boolean showDialog) {
 		numVideosFetched = 0;
 		numChannelsFetched = 0;
-		numChannelsSubscribed = SubscriptionsDb.getSubscriptionsDb().numSubscribedChannels();
+		numChannelsSubscribed = SubscriptionsDb.getSubscriptionsDb().getTotalSubscribedChannels();
 		if(numChannelsSubscribed > 0) {
 			new GetSubscriptionVideosTask(this).executeInParallel();
 			refreshInProgress = true;
@@ -110,6 +94,7 @@ public class SubscriptionsFragment extends VideosGridFragment implements Subscri
 			}
 		}
 	}
+
 
 	private void showRefreshDialog() {
 		progressDialog = new MaterialDialog.Builder(getActivity())
@@ -123,10 +108,12 @@ public class SubscriptionsFragment extends VideosGridFragment implements Subscri
 		progressDialog.show();
 	}
 
+
 	@Override
 	public void onRefresh() {
 		doRefresh(true);
 	}
+
 
 	@Override
 	public void onChannelVideosFetched(YouTubeChannel channel, int videosFetched, final boolean videosDeleted) {
@@ -164,11 +151,24 @@ public class SubscriptionsFragment extends VideosGridFragment implements Subscri
 		}
 	}
 
-	/**
-	 * When the Subscriptions tab is selected, if a refresh is in progress, show the dialog.
-	 */
-	public void onSelected() {
-		if(refreshInProgress)
+
+	@Override
+	protected void onFragmentSelected() {
+		// when the Subscriptions tab is selected, if a refresh is in progress, show the dialog.
+		if (refreshInProgress)
 			showRefreshDialog();
 	}
+
+
+	@Override
+	protected VideoCategory getVideoCategory() {
+		return VideoCategory.SUBSCRIPTIONS_FEED_VIDEOS;
+	}
+
+
+	@Override
+	protected String getFragmentName() {
+		return SkyTubeApp.getStr(R.string.subscriptions);
+	}
+
 }

app/src/main/java/free/rm/skytube/businessobjects/GetBookmarksVideos.java
@@ -1,0 +1,51 @@
+/*
+ * SkyTube
+ * Copyright (C) 2017  Ramon Mifsud
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation (version 3 of the License).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package free.rm.skytube.businessobjects;
+
+import java.io.IOException;
+import java.util.List;
+
+import free.rm.skytube.businessobjects.db.BookmarksDb;
+
+
+/**
+ * Get user's bookmarked videos.
+ */
+public class GetBookmarksVideos extends GetYouTubeVideos {
+
+	@Override
+	public void init() throws IOException {
+		noMoreVideoPages = false;
+	}
+
+	@Override
+	public List<YouTubeVideo> getNextVideos() {
+		if (!noMoreVideoPages()) {
+			noMoreVideoPages = true;
+			return BookmarksDb.getBookmarksDb().getBookmarkedVideos();
+		}
+
+		return null;
+	}
+
+	@Override
+	public boolean noMoreVideoPages() {
+		return noMoreVideoPages;
+	}
+
+}

app/src/main/java/free/rm/skytube/businessobjects/GetSubscriptionsVideos.java
@@ -1,0 +1,53 @@
+/*
+ * SkyTube
+ * Copyright (C) 2017  Ramon Mifsud
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation (version 3 of the License).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package free.rm.skytube.businessobjects;
+
+import java.io.IOException;
+import java.util.List;
+
+import free.rm.skytube.businessobjects.db.SubscriptionsDb;
+
+
+/**
+ * Get user's subscriptions video feed.
+ */
+public class GetSubscriptionsVideos extends GetYouTubeVideos {
+
+	@Override
+	public void init() throws IOException {
+		noMoreVideoPages = false;
+	}
+
+
+	@Override
+	public List<YouTubeVideo> getNextVideos() {
+		if (!noMoreVideoPages()) {
+			noMoreVideoPages = true;
+			return SubscriptionsDb.getSubscriptionsDb().getSubscriptionVideos();
+		}
+
+		return null;
+	}
+
+
+	@Override
+	public boolean noMoreVideoPages() {
+		return noMoreVideoPages;
+	}
+
+}

app/src/main/java/free/rm/skytube/businessobjects/VideoCategory.java
@@ -27,16 +27,16 @@ import free.rm.skytube.businessobjects.VideoStream.GetChannelVideos;
 public enum VideoCategory {
 	/** Featured videos */
 	FEATURED (0),
-
 	/** Most popular videos */
 	MOST_POPULAR (1),
-
 	/** Videos related to a search query */
 	SEARCH_QUERY (2),
-
 	/** Videos that are owned by a channel */
-	CHANNEL_VIDEOS (3);
-
+	CHANNEL_VIDEOS (3),
+	/** Videos pertaining to the user's subscriptions feed */
+	SUBSCRIPTIONS_FEED_VIDEOS (4),
+	/** Videos bookmarked by the user */
+	BOOKMARKS_VIDEOS (5);
 
 	// *****************
 	// DON'T FORGET to update getVideoCategory() and createGetYouTubeVideos() methods...
@@ -44,6 +44,7 @@ public enum VideoCategory {
 
 	private final int id;
 	private static final String TAG = VideoCategory.class.getSimpleName();
+
 
 
 	VideoCategory(int id) {
@@ -85,8 +86,13 @@ public enum VideoCategory {
 			return new GetYouTubeVideoBySearch();
 		else if (id == CHANNEL_VIDEOS.id)
 			return new GetChannelVideos();
+		else if (id == SUBSCRIPTIONS_FEED_VIDEOS.id)
+			return new GetSubscriptionsVideos();
+		else if (id == BOOKMARKS_VIDEOS.id)
+			return new GetBookmarksVideos();
 
-		return null;
+		// this will notify the developer is he forgot to amend this method when a new type is added
+		throw new UnsupportedOperationException();
 	}
 
 }

app/src/main/java/free/rm/skytube/businessobjects/db/BookmarksDb.java
@@ -42,11 +42,7 @@ public class BookmarksDb extends SQLiteOpenHelper {
 	private static final int DATABASE_VERSION = 1;
 	private static final String DATABASE_NAME = "bookmarks.db";
 
-	private List<SavedVideosDbListener> listeners = new ArrayList<>();
-
-	public interface SavedVideosDbListener {
-		void onUpdated();
-	}
+	private List<BookmarksDbListener> listeners = new ArrayList<>();
 
 
 	private BookmarksDb(Context context) {
@@ -199,11 +195,11 @@ public class BookmarksDb extends SQLiteOpenHelper {
 
 
 	/**
-	 * Get the list of Videos that have been saved.
+	 * Get the list of Videos that have been bookmarked.
 	 *
 	 * @return List of Videos
 	 */
-	public List<YouTubeVideo> getSavedVideos() {
+	public List<YouTubeVideo> getBookmarkedVideos() {
 		Cursor	cursor = getReadableDatabase().query(
 						BookmarksTable.TABLE_NAME,
 						new String[]{BookmarksTable.COL_YOUTUBE_VIDEO, BookmarksTable.COL_ORDER},
@@ -227,15 +223,15 @@ public class BookmarksDb extends SQLiteOpenHelper {
 	 *
 	 * @param listener The Listener (which implements SavedVideosDbListener) to add.
 	 */
-	public void addListener(SavedVideosDbListener listener) {
+	public void addListener(BookmarksDbListener listener) {
 		if(!listeners.contains(listener))
 			listeners.add(listener);
 	}
 
 	private void onUpdated() {
 		hasUpdated = true;
-		for(SavedVideosDbListener listener : listeners) {
-			listener.onUpdated();
+		for(BookmarksDbListener listener : listeners) {
+			listener.onBookmarksDbUpdated();
 		}
 	}
 
@@ -246,4 +242,18 @@ public class BookmarksDb extends SQLiteOpenHelper {
 	public static void setHasUpdated(boolean hasUpdated) {
 		BookmarksDb.hasUpdated = hasUpdated;
 	}
+
+
+
+	////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+	public interface BookmarksDbListener {
+		/**
+		 * Will be called once the bookmarks DB is updated (by either a bookmark insertion or
+		 * deletion).
+		 */
+		void onBookmarksDbUpdated();
+	}
+
 }

app/src/main/java/free/rm/skytube/businessobjects/db/SubscriptionsDb.java
@@ -127,14 +127,20 @@ public class SubscriptionsDb extends SQLiteOpenHelper {
 		return subsChannels;
 	}
 
-	public int numSubscribedChannels() {
-		String query = String.format("SELECT COUNT(*) FROM %s", SubscriptionsTable.TABLE_NAME);
-		Cursor cursor = SubscriptionsDb.getSubscriptionsDb().getReadableDatabase().rawQuery(query, null);
 
-		if(cursor.moveToFirst()) {
-			return cursor.getInt(0);
-		}
-		return 0;
+	/**
+	 * @return The total number of subscribed channels.
+	 */
+	public int getTotalSubscribedChannels() {
+		String	query = String.format("SELECT COUNT(*) FROM %s", SubscriptionsTable.TABLE_NAME);
+		Cursor	cursor = SubscriptionsDb.getSubscriptionsDb().getReadableDatabase().rawQuery(query, null);
+		int		totalSubbedChannels = 0;
+
+		if (cursor.moveToFirst())
+			totalSubbedChannels = cursor.getInt(0);
+
+		cursor.close();
+		return totalSubbedChannels;
 	}
 
 

app/src/main/java/free/rm/skytube/gui/businessobjects/VideoGridAdapter.java
@@ -146,6 +146,14 @@ public class VideoGridAdapter extends RecyclerViewAdapterEx<YouTubeVideo, GridVi
 
 	/**
 	 * Refresh the video grid, by running the task to get the videos again.
+	 */
+	public void refresh() {
+		refresh(null);
+	}
+
+
+	/**
+	 * Refresh the video grid, by running the task to get the videos again.
 	 *
 	 * @param onFinished Runnable to run when the task completes.
 	 */

commit 3cb07d3caeb907a3a0430927875831bf7fcb28b5

Fixed a small bug caused by commit c8d27cedcdf0c2eae6dc1fa2822450d562ee8d54 which caused the videos to disappear when the ChannelBrowser is launched and then the user goes back to the BookmarksFragment or SubscriptionsFragment.

app/src/main/java/free/rm/skytube/gui/fragments/BookmarksFragment.java
@@ -57,7 +57,6 @@ public class BookmarksFragment extends VideosGridFragment implements BookmarksDb
 		if (bookmarksGridAdapter == null)
 			bookmarksGridAdapter = new bookmarksGridAdapter(getActivity());
 
-		bookmarksGridAdapter.clearList();
 		bookmarksGridAdapter.setListener((MainActivityListener)getActivity());
 		gridView.setAdapter(bookmarksGridAdapter);
 

app/src/main/java/free/rm/skytube/gui/fragments/SubscriptionsFragment.java
@@ -60,10 +60,10 @@ public class SubscriptionsFragment extends VideosGridFragment implements Subscri
 	@Override
 	public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
 		super.onViewCreated(view, savedInstanceState);
-		videoGridAdapter.clearList();
 
 		numChannelsSubscribed = SubscriptionsDb.getSubscriptionsDb().getTotalSubscribedChannels();
-		if(numChannelsSubscribed == 0) {
+
+		if (numChannelsSubscribed <= 0) {
 			swipeRefreshLayout.setVisibility(View.GONE);
 			noSubscriptionsText.setVisibility(View.VISIBLE);
 		} else {