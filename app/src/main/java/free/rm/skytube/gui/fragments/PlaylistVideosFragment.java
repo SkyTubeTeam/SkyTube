57d65358698937b5b226530f967e0808b00345f7
Ramon M <ramon.mifsud@yandex.com>
2017-11-25 12:46:06 +0100

    Code simplification wrt PlaylistVideosFragment.

--- app/src/main/java/free/rm/skytube/gui/fragments/PlaylistVideosFragment.java
+++ app/src/main/java/free/rm/skytube/gui/fragments/PlaylistVideosFragment.java
@@ -2,7 +2,6 @@ package free.rm.skytube.gui.fragments;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
-import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,11 +17,9 @@ import butterknife.ButterKnife;
 import free.rm.skytube.R;
 import free.rm.skytube.businessobjects.VideoCategory;
 import free.rm.skytube.businessobjects.YouTubePlaylist;
-import free.rm.skytube.gui.businessobjects.LoadingProgressBar;
-import free.rm.skytube.gui.businessobjects.VideoGridAdapter;
 
 /**
- * A Fragment that displays the Videos of a Playlist in a {@link VideosGridFragment}
+ * A Fragment that displays the videos of a playlist in a {@link VideosGridFragment}
  */
 public class PlaylistVideosFragment extends VideosGridFragment {
 	private YouTubePlaylist youTubePlaylist;
@@ -30,35 +27,24 @@ public class PlaylistVideosFragment extends VideosGridFragment {
 	public static final String PLAYLIST_OBJ = "PlaylistVideosFragment.PLAYLIST_OBJ";
 
 	@BindView(R.id.toolbar)
-	Toolbar toolbar;
+	Toolbar     toolbar;
 	@BindView(R.id.playlist_banner_image_view)
-	ImageView playlistBannerImageView;
+	ImageView   playlistBannerImageView;
 	@BindView(R.id.playlist_title_text_view)
-	TextView playlistTitleTextView;
+	TextView    playlistTitleTextView;
 
-	@Override
-	public String getFragmentName() {
-		return null;
-	}
-
-	@Override
-	protected VideoCategory getVideoCategory() {
-		return VideoCategory.PLAYLIST_VIDEOS;
-	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.fragment_playlist_videos, container, false);
-
-		ButterKnife.bind(this, view);
-
-		gridView = view.findViewById(R.id.grid_view);
-
-		// set up the loading progress bar
-		LoadingProgressBar.get().setProgressBar(view.findViewById(R.id.loading_progress_bar));
-
+		// sets the play list
 		youTubePlaylist = (YouTubePlaylist)getArguments().getSerializable(PLAYLIST_OBJ);
 
+		// sets the layout resource which is used by onCreateView()
+		setLayoutResource(R.layout.fragment_playlist_videos);
+
+		View view = super.onCreateView(inflater, container, savedInstanceState);
+
+		ButterKnife.bind(this, view);
 		playlistTitleTextView.setText(youTubePlaylist.getTitle());
 
 		// setup the toolbar/actionbar
@@ -70,20 +56,31 @@ public class PlaylistVideosFragment extends VideosGridFragment {
 			actionBar.setTitle(youTubePlaylist.getChannel().getTitle());
 		}
 
+		// set the channel's banner
 		Glide.with(getActivity())
-						.load(youTubePlaylist.getBannerUrl())
-						.apply(new RequestOptions().placeholder(R.drawable.banner_default))
-						.into(playlistBannerImageView);
-
-		if (videoGridAdapter == null) {
-			videoGridAdapter = new VideoGridAdapter(getActivity(), false /*hide channel name*/);
-			videoGridAdapter.setVideoCategory(VideoCategory.PLAYLIST_VIDEOS, youTubePlaylist.getId());
-		}
-
-		gridView.setHasFixedSize(false);
-		gridView.setLayoutManager(new GridLayoutManager(getActivity(), getResources().getInteger(R.integer.video_grid_num_columns)));
-		gridView.setAdapter(videoGridAdapter);
+				.load(youTubePlaylist.getBannerUrl())
+				.apply(new RequestOptions().placeholder(R.drawable.banner_default))
+				.into(playlistBannerImageView);
 
 		return view;
 	}
+
+
+	@Override
+	public String getFragmentName() {
+		return null;
+	}
+
+
+	@Override
+	protected VideoCategory getVideoCategory() {
+		return VideoCategory.PLAYLIST_VIDEOS;
+	}
+
+
+	@Override
+	protected String getSearchString() {
+		return youTubePlaylist.getId();
+	}
+
 }
