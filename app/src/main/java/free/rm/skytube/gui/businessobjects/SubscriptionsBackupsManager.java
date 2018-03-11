97efbaf3a6cd1ca1ce16c29c5e6ad8748c9df2d4
Ramon <ramon.mifsud@yandex.com>
2018-03-11 15:48:58 +0100

    Update SubscriptionsBackupsManager.java
    Fixed a bug which subscriptions were removed BEFORE the user clicked on the "Import Subs" button.

--- app/src/main/java/free/rm/skytube/gui/businessobjects/SubscriptionsBackupsManager.java
+++ app/src/main/java/free/rm/skytube/gui/businessobjects/SubscriptionsBackupsManager.java
@@ -44,6 +44,7 @@ import free.rm.skytube.businessobjects.AsyncTaskParallel;
 import free.rm.skytube.businessobjects.Logger;
 import free.rm.skytube.businessobjects.YouTube.Tasks.GetSubscriptionVideosTask;
 import free.rm.skytube.businessobjects.db.SubscriptionsDb;
+import free.rm.skytube.businessobjects.db.Tasks.UnsubscribeFromAllChannelsTask;
 import free.rm.skytube.gui.businessobjects.adapters.ImportSubscriptionsAdapter;
 import free.rm.skytube.gui.businessobjects.adapters.SubsAdapter;
 import free.rm.skytube.gui.businessobjects.preferences.BackupDatabases;
@@ -128,9 +129,6 @@ public class SubscriptionsBackupsManager {
 					if (importDb)
 						displayImportDbsBackupWarningMsg(files[0]);
 					else {
-						if (isUnsubsribeAllChecked) {
-							SubscriptionsDb.getSubscriptionsDb().unsubscribeFromAllChannels();
-						}
 						Uri uri = Uri.fromFile(new File(files[0]));
 						parseImportedSubscriptions(uri);
 					}
@@ -313,6 +311,11 @@ public class SubscriptionsBackupsManager {
 								.onPositive(new MaterialDialog.SingleButtonCallback() {
 									@Override
 									public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+										// if the user checked the "Unsubscribe to all subscribed channels" checkbox
+										if (isUnsubsribeAllChecked) {
+											new UnsubscribeFromAllChannelsTask().executeInParallel();
+										}
+
 										List<ImportSubscriptionsChannel> channelsToSubscribeTo = new ArrayList<>();
 										for(ImportSubscriptionsChannel channel: channels) {
 											if(channel.isChecked)
