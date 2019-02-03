d9fb3daa48a64c110ae34febc1d3d4e5b8bfd330
Ramon M <ramon.mifsud@yandex.com>
2019-02-03 15:50:57 +0100

    Updated the glide library.

---
+++ app/src/main/java/free/rm/skytube/gui/SkyTubeGlideModule.java
@@ -0,0 +1,25 @@
+/*
+ * SkyTube
+ * Copyright (C) 2019  Ramon Mifsud
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation (version 3 of the License).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package free.rm.skytube.gui;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public final class SkyTubeGlideModule extends AppGlideModule {
+}
