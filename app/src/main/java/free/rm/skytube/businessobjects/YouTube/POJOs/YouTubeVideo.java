1d92614c19d5ba52cc25e3fd1ecab475b842a2eb
Zsombor Gegesy <gzsombor@gmail.com>
2019-08-16 00:23:33 +0200

    Remove parent folder after downloaded file removal

--- app/src/main/java/free/rm/skytube/businessobjects/YouTube/POJOs/YouTubeVideo.java
+++ app/src/main/java/free/rm/skytube/businessobjects/YouTube/POJOs/YouTubeVideo.java
@@ -499,7 +499,23 @@ public class YouTubeVideo extends CardData implements Serializable {
 		if (file.exists()) {
 			file.delete();
 		}
+		if (SkyTubeApp.getSettings().isDownloadToSeparateFolders()) {
+			removeParentFolderIfEmpty(file);
+		}
 		DownloadedVideosDb.getVideoDownloadsDb().remove(YouTubeVideo.this);
+	}
+
+	private void removeParentFolderIfEmpty(File file) {
+		File parentFile = file.getParentFile();
+		if (parentFile.exists() && parentFile.isDirectory()) {
+			String[] fileList = parentFile.list();
+			if (fileList != null) {
+				if (fileList.length == 0) {
+					// that was the last file in the directory, remove it
+					parentFile.delete();
+				}
+			}
+		}
 	}
 
 
