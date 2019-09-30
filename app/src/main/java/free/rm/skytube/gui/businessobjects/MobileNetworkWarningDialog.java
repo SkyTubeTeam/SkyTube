3cd847f5b3e976211154b5eaa5589e8207bd12ba
Zsombor Gegesy <gzsombor@gmail.com>
2019-10-01 01:19:11 +0200

    Implement ask/allow/block policy for mobile network usage

--- app/src/main/java/free/rm/skytube/gui/businessobjects/MobileNetworkWarningDialog.java
+++ app/src/main/java/free/rm/skytube/gui/businessobjects/MobileNetworkWarningDialog.java
@@ -22,12 +22,12 @@ import android.content.Context;
 import androidx.annotation.NonNull;
 
 import android.content.DialogInterface;
-import android.widget.CompoundButton;
 
 import com.afollestad.materialdialogs.MaterialDialog;
 
 import free.rm.skytube.R;
 import free.rm.skytube.app.SkyTubeApp;
+import free.rm.skytube.app.enums.Policy;
 import free.rm.skytube.businessobjects.YouTube.POJOs.YouTubeVideo;
 
 /**
@@ -51,15 +51,26 @@ public class MobileNetworkWarningDialog extends SkyTubeMaterialDialog {
 	 * @return True if the dialog was displayed; false otherwise.
 	 */
 	public boolean showAndGetStatus(ActionType actionType) {
-		final boolean   displayWarning  = SkyTubeApp.getPreferenceManager().getBoolean(SkyTubeApp.getStr(R.string.pref_key_warn_mobile_downloads), true);
+		final Policy displayWarning  = SkyTubeApp.getSettings().getWarningMobilePolicy();
 
-		if (SkyTubeApp.isConnectedToMobile() && displayWarning) {
-			title(R.string.mobile_data);
-			content(actionType == ActionType.STREAM_VIDEO ? R.string.warning_mobile_network_play : R.string.warning_mobile_network_download);
-			checkBoxPromptRes(R.string.warning_mobile_network_disable, false, (buttonView, isChecked) -> SkyTubeApp.getPreferenceManager().edit().putBoolean(SkyTubeApp.getStr(R.string.pref_key_warn_mobile_downloads), !isChecked).apply());
-			positiveText(actionType == ActionType.STREAM_VIDEO ?  R.string.play_video : R.string.download_video);
-			show();
-			return true;
+		if (SkyTubeApp.isConnectedToMobile()) {
+			switch (displayWarning) {
+				case BLOCK : {
+					cancelListener.onCancel(null);
+					return false;
+				}
+				case ALLOW : {
+					return false;
+				}
+				case ASK : {
+					title(R.string.mobile_data);
+					content(actionType == ActionType.STREAM_VIDEO ? R.string.warning_mobile_network_play : R.string.warning_mobile_network_download);
+					checkBoxPromptRes(R.string.warning_mobile_network_disable, false, null);
+					positiveText(actionType == ActionType.STREAM_VIDEO ?  R.string.play_video : R.string.download_video);
+					show();
+					return true;
+				}
+			}
 		}
 
 		return false;
@@ -77,13 +88,23 @@ public class MobileNetworkWarningDialog extends SkyTubeMaterialDialog {
 
 	@Override
 	public MobileNetworkWarningDialog onPositive(@NonNull MaterialDialog.SingleButtonCallback callback) {
-		this.onPositiveCallback = callback;
+		this.onPositiveCallback = (dialog, action) -> {
+            if (dialog.isPromptCheckBoxChecked()) {
+                SkyTubeApp.getSettings().setWarningMobilePolicy(Policy.ALLOW);
+            }
+            callback.onClick(dialog, action);
+        };
 		return this;
 	}
 
 	@Override
 	public MobileNetworkWarningDialog onNegativeOrCancel(@NonNull DialogInterface.OnCancelListener callback) {
-		this.onNegativeCallback = (dialog, action) -> callback.onCancel(dialog);
+		this.onNegativeCallback = (dialog, action) -> {
+		    if (dialog.isPromptCheckBoxChecked()) {
+                SkyTubeApp.getSettings().setWarningMobilePolicy(Policy.BLOCK);
+            }
+            callback.onCancel(dialog);
+        };
 		this.cancelListener = callback;
 		return this;
 	}
