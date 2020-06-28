8050a7cb18384d9e67795c0db6208a91a1e70fa3
Zsombor Gegesy <gzsombor@gmail.com>
2020-06-29 01:38:24 +0200

    Obfuscate API keys - so the source code scanner doesnt warn about insecure usage. It's still insecure

---
+++ app/src/main/java/free/rm/skytube/businessobjects/YouTube/POJOs/ObfuscateKey.java
@@ -0,0 +1,105 @@
+/*
+ * SkyTube
+ * Copyright (C) 2020  Zsombor Gegesy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation (version 3 of the License).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package free.rm.skytube.businessobjects.YouTube.POJOs;
+
+import com.google.common.base.Charsets;
+
+/**
+ * Very simple RC4 obfuscator, to hide the API key from the source code scanners.
+ * Of course, this is not a real measure against anything serious.
+ */
+public class ObfuscateKey {
+    private final byte[] S = new byte[256];
+    private final byte[] T = new byte[256];
+    private final int keylen;
+
+    public ObfuscateKey(final byte[] key) {
+        if (key.length < 1 || key.length > 256) {
+            throw new IllegalArgumentException(
+                    "key must be between 1 and 256 bytes");
+        } else {
+            keylen = key.length;
+            for (int i = 0; i < 256; i++) {
+                S[i] = (byte) i;
+                T[i] = key[i % keylen];
+            }
+            int j = 0;
+            byte tmp;
+            for (int i = 0; i < 256; i++) {
+                j = (j + S[i] + T[i]) & 0xFF;
+                tmp = S[j];
+                S[j] = S[i];
+                S[i] = tmp;
+            }
+        }
+    }
+
+    public ObfuscateKey(String key) {
+        this(key.getBytes(Charsets.ISO_8859_1));
+    }
+
+    private byte[] encrypt(final byte[] plaintext) {
+        final byte[] ciphertext = new byte[plaintext.length];
+        int i = 0, j = 0, k, t;
+        byte tmp;
+        for (int counter = 0; counter < plaintext.length; counter++) {
+            i = (i + 1) & 0xFF;
+            j = (j + S[i]) & 0xFF;
+            tmp = S[j];
+            S[j] = S[i];
+            S[i] = tmp;
+            t = (S[i] + S[j]) & 0xFF;
+            k = S[t];
+            ciphertext[counter] = (byte) (plaintext[counter] ^ k);
+        }
+        return ciphertext;
+    }
+
+    public String decrypt(String input) {
+        byte[] raw = encrypt(parseHexBinary(input));
+        return new String(raw, Charsets.ISO_8859_1);
+    }
+
+
+    private byte[] parseHexBinary(String s) {
+        final int len = s.length();
+
+        // "111" is not a valid hex encoding.
+        if( len%2 != 0 )
+            throw new IllegalArgumentException("hexBinary needs to be even-length: "+s);
+
+        byte[] out = new byte[len/2];
+
+        for( int i=0; i<len; i+=2 ) {
+            int h = hexToBin(s.charAt(i  ));
+            int l = hexToBin(s.charAt(i+1));
+            if( h==-1 || l==-1 )
+                throw new IllegalArgumentException("contains illegal character for hexBinary: "+s);
+
+            out[i/2] = (byte)(h*16+l);
+        }
+
+        return out;
+    }
+
+    private static int hexToBin( char ch ) {
+        if( '0'<=ch && ch<='9' )    return ch-'0';
+        if( 'A'<=ch && ch<='F' )    return ch-'A'+10;
+        if( 'a'<=ch && ch<='f' )    return ch-'a'+10;
+        return -1;
+    }
+}
