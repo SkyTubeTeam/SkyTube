7b4a20291b978c325caf1f8e565b9d54b0a8edd8
Isira Seneviratne <isirasen96@gmail.com>
2020-12-13 10:31:51 +0530

    Use view binding in YouTubePlayerTutorialFragment.

--- app/src/main/java/free/rm/skytube/gui/fragments/YouTubePlayerTutorialFragment.java
+++ app/src/main/java/free/rm/skytube/gui/fragments/YouTubePlayerTutorialFragment.java
@@ -25,26 +25,25 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.core.content.ContextCompat;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import free.rm.skytube.R;
 import free.rm.skytube.app.SkyTubeApp;
+import free.rm.skytube.databinding.FragmentYoutubePlayerTutorialBinding;
 import free.rm.skytube.gui.businessobjects.fragments.ImmersiveModeFragment;
 
 /**
  * Fragment that will display the tutorial for the YouTube Player.
  */
 public class YouTubePlayerTutorialFragment extends ImmersiveModeFragment implements ViewPager.OnPageChangeListener {
+	private static final int[] tutorialSlideViews = {R.layout.tutorial_player_1, R.layout.tutorial_player_2,
+			R.layout.tutorial_player_3, R.layout.tutorial_player_4, R.layout.tutorial_player_5,
+			R.layout.tutorial_player_6};
 
-	private YouTubePlayerTutorialListener   listener = null;
-	private ViewPager                       viewPager;
-	private TextView                        nextTextView;
-	private TextView                        pageCounterTextView;
+	private YouTubePlayerTutorialListener listener = null;
+	private FragmentYoutubePlayerTutorialBinding binding;
 	/** Tutorial slides layout resources. */
-	private final static int[] tutorialSlideViews = {R.layout.tutorial_player_1, R.layout.tutorial_player_2, R.layout.tutorial_player_3, R.layout.tutorial_player_4, R.layout.tutorial_player_5, R.layout.tutorial_player_6};
-
 
 	@Nullable
 	@Override
@@ -52,33 +51,35 @@ public class YouTubePlayerTutorialFragment extends ImmersiveModeFragment impleme
 		hideNavigationBar();
 
 		// inflate the layout for this fragment
-		View view = inflater.inflate(R.layout.fragment_youtube_player_tutorial, container, false);
+		binding = FragmentYoutubePlayerTutorialBinding.inflate(inflater, container, false);
 
-		nextTextView = view.findViewById(R.id.nextButton);
-		nextTextView.setOnClickListener(v -> {
-			if (viewPager.getCurrentItem() < tutorialSlideViews.length-1) {
-				viewPager.setCurrentItem(viewPager.getCurrentItem()+1);
+		binding.nextButton.setOnClickListener(v -> {
+			if (binding.pager.getCurrentItem() < tutorialSlideViews.length-1) {
+				binding.pager.setCurrentItem(binding.pager.getCurrentItem()+1);
 			} else {
 				// the user wants to end the tutorial (by clicking the 'done' button)
 				if (listener != null)
 					listener.onTutorialFinished();
 			}
 		});
-		view.findViewById(R.id.skipButton).setOnClickListener(v -> {
+		binding.skipButton.setOnClickListener(v -> {
 			// the user wants to end the tutorial (by clicking the 'skip' button)
 			if (listener != null)
 				listener.onTutorialFinished();
 		});
-		pageCounterTextView = view.findViewById(R.id.pageCounterTextView);
-		viewPager = view.findViewById(R.id.pager);
-		viewPager.setAdapter(new TutorialPagerAdapter());
-		viewPager.addOnPageChangeListener(this);
+		binding.pager.setAdapter(new TutorialPagerAdapter());
+		binding.pager.addOnPageChangeListener(this);
 
 		setUp(0);
 
-		return view;
+		return binding.getRoot();
 	}
 
+	@Override
+	public void onDestroyView() {
+		binding = null;
+		super.onDestroyView();
+	}
 
 	/**
 	 * Setup the {@link YouTubePlayerTutorialListener} which will be called once the user wants to
@@ -89,11 +90,9 @@ public class YouTubePlayerTutorialFragment extends ImmersiveModeFragment impleme
 		return this;
 	}
 
-
 	@Override
 	public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 	}
-
 
 	/**
 	 * Set up the nextTextView and pageCounterTextView's text content according to the slide
@@ -103,10 +102,10 @@ public class YouTubePlayerTutorialFragment extends ImmersiveModeFragment impleme
 	 * @param position  slide position as per {@link #tutorialSlideViews}.
 	 */
 	private void setUp(int position) {
-		nextTextView.setText((position == tutorialSlideViews.length - 1) ? R.string.done : R.string.next);
-		pageCounterTextView.setText(getString(R.string.page_counter, position + 1, tutorialSlideViews.length));
+		binding.nextButton.setText((position == tutorialSlideViews.length - 1) ? R.string.done : R.string.next);
+		binding.pageCounterTextView.setText(getString(R.string.page_counter,
+				position + 1, tutorialSlideViews.length));
 	}
-
 
 	@Override
 	public void onPageSelected(int position) {
@@ -127,18 +126,18 @@ public class YouTubePlayerTutorialFragment extends ImmersiveModeFragment impleme
 		@Override
 		public Object instantiateItem(@NonNull ViewGroup container, int position) {
 			// inflate the tutorial slide layout resource according to the give position...
-			LayoutInflater inflater = ContextCompat.getSystemService(getActivity(), LayoutInflater.class);
+			LayoutInflater inflater = LayoutInflater.from(requireContext());
 			View tutorialSlideView = inflater.inflate(tutorialSlideViews[position], null);
 
 			configureBrightnessVolumeLabels(tutorialSlideView);
 
-			viewPager.addView(tutorialSlideView);
+			binding.pager.addView(tutorialSlideView);
 			return tutorialSlideView;
 		}
 
 		@Override
 		public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
-			viewPager.removeView((View)object);
+			binding.pager.removeView((View)object);
 		}
 
 		@Override
@@ -169,19 +168,14 @@ public class YouTubePlayerTutorialFragment extends ImmersiveModeFragment impleme
 				}
 			}
 		}
-
 	}
-
 
 	////////////////////////////////////////////////////////////////////////////////////////////////
 
 	public interface YouTubePlayerTutorialListener {
-
 		/**
 		 * Will be called once the tutorial is finished.
 		 */
 		void onTutorialFinished();
-
 	}
-
 }
