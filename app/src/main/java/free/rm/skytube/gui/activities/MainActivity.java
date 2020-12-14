cd42d6587afebed200571500382ac0b20d49d7ae
Isira Seneviratne <isirasen96@gmail.com>
2020-12-14 14:42:02 +0530

    Replace preference PNGs with vector drawables.
    Also use a network drawable for the network preference header.

--- app/src/main/java/free/rm/skytube/gui/activities/MainActivity.java
+++ app/src/main/java/free/rm/skytube/gui/activities/MainActivity.java
@@ -22,6 +22,7 @@ import android.content.ClipboardManager;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
@@ -34,8 +35,10 @@ import android.widget.FrameLayout;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
 import androidx.appcompat.widget.SearchView;
 import androidx.core.content.ContextCompat;
+import androidx.core.graphics.drawable.DrawableCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -541,21 +544,22 @@ public class MainActivity extends BaseActivity {
 		 * Setup the video blocker notification icon which will be displayed in the tool bar.
  		 */
 		void setupIconForToolBar(final Menu menu) {
+			final Drawable blocker = AppCompatResources.getDrawable(activity, R.drawable.ic_blocker)
+					.mutate();
+			DrawableCompat.setTint(blocker, Color.WHITE);
 			if (getTotalBlockedVideos() > 0) {
 				// display a red bubble containing the number of blocked videos
-				ActionItemBadge.update(activity,
-						menu.findItem(R.id.menu_blocker),
-						ContextCompat.getDrawable(activity, R.drawable.ic_video_blocker),
-						ActionItemBadge.BadgeStyles.RED,
-						getTotalBlockedVideos());
+				ActionItemBadge.update(activity, menu.findItem(R.id.menu_blocker), blocker,
+						ActionItemBadge.BadgeStyles.RED, getTotalBlockedVideos());
 			} else {
 				// Else, set the bubble to transparent.  This is required so that when the user
 				// clicks on the icon, the app will be able to detect such click and displays the
 				// BlockedVideosDialog (otherwise, the ActionItemBadge would just ignore such clicks.
 				ActionItemBadge.update(activity,
-						menu.findItem(R.id.menu_blocker),
-						ContextCompat.getDrawable(activity, R.drawable.ic_video_blocker),
-						new BadgeStyle(BadgeStyle.Style.DEFAULT, com.mikepenz.actionitembadge.library.R.layout.menu_action_item_badge, Color.TRANSPARENT, Color.TRANSPARENT, Color.WHITE),
+						menu.findItem(R.id.menu_blocker), blocker,
+						new BadgeStyle(BadgeStyle.Style.DEFAULT,
+								com.mikepenz.actionitembadge.library.R.layout.menu_action_item_badge,
+								Color.TRANSPARENT, Color.TRANSPARENT, Color.WHITE),
 						"");
 			}
 		}
