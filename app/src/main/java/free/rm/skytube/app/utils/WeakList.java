ddd21d8065c3ee3167796a9df23885a2746a34b0
Zsombor Gegesy <gzsombor@gmail.com>
2021-02-10 23:53:57 +0100

    Allow to use the system default country - add notification about changes

---
+++ app/src/main/java/free/rm/skytube/app/utils/WeakList.java
@@ -0,0 +1,50 @@
+/*
+ * SkyTube
+ * Copyright (C) 2021  Zsombor Gegesy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation (version 3 of the License).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package free.rm.skytube.app.utils;
+
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class WeakList<X> {
+    private List<WeakReference<X>> values = new ArrayList<>();
+
+    public void add(X object) {
+        cleanup(null);
+        values.add(new WeakReference<>(object));
+    }
+
+    public void forEach(Consumer<X> consumer) {
+        cleanup(consumer);
+    }
+
+    private void cleanup(Consumer<X> consumer) {
+        for (Iterator<WeakReference<X>> iter = values.iterator(); iter.hasNext(); ) {
+            WeakReference<X> ref = iter.next();
+            X value = ref.get();
+            if (value == null) {
+                iter.remove();
+            } else {
+                if (consumer != null) {
+                    consumer.accept(value);
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/free/rm/skytube/gui/fragments/preferences/VideoPlayerPreferenceFragment.java
+++ app/src/main/java/free/rm/skytube/gui/fragments/preferences/VideoPlayerPreferenceFragment.java
@@ -17,6 +17,7 @@
 
 package free.rm.skytube.gui.fragments.preferences;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.preference.ListPreference;
@@ -27,13 +28,15 @@ import java.util.Arrays;
 
 import free.rm.skytube.BuildConfig;
 import free.rm.skytube.R;
+import free.rm.skytube.app.EventBus;
 import free.rm.skytube.app.SkyTubeApp;
+import free.rm.skytube.businessobjects.Logger;
 import free.rm.skytube.businessobjects.YouTube.newpipe.NewPipeService;
 
 /**
  * Preference fragment for video player related settings.
  */
-public class VideoPlayerPreferenceFragment extends PreferenceFragmentCompat {
+public class VideoPlayerPreferenceFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
 	@Override
 	public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
 		addPreferencesFromResource(R.xml.preference_video_player);
@@ -55,19 +58,38 @@ public class VideoPlayerPreferenceFragment extends PreferenceFragmentCompat {
 		});
 
 		configureCountrySelector();
+
+	}
+	@Override
+	public void onResume() {
+		super.onResume();
+		getPreferenceManager().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+	}
+
+	@Override
+	public void onPause() {
+		getPreferenceManager().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+		super.onPause();
 	}
 
 	private void configureCountrySelector() {
 		ListPreference countrySelector = findPreference(getString(R.string.pref_key_default_content_country));
 		String[] countryCodes = SkyTubeApp.getStringArray(R.array.country_codes);
 		String[] countryNames = SkyTubeApp.getStringArray(R.array.country_names);
-		countrySelector.setEntryValues(Arrays.copyOfRange(countryCodes, 1, countryCodes.length));
-		countrySelector.setEntries(Arrays.copyOfRange(countryNames, 1, countryNames.length));
-
-		countrySelector.setOnPreferenceChangeListener((preference, newValue) -> {
-			NewPipeService.setCountry((String) newValue);
-			return true;
-		});
+		countrySelector.setEntryValues(countryCodes);
+		String[] countryNamesWithSystemDefault = new String[countryNames.length];
+		System.arraycopy(countryNames, 1, countryNamesWithSystemDefault, 1, countryNames.length - 1);
+		countryNamesWithSystemDefault[0] = getString(R.string.system_default_country);
+		countrySelector.setEntries(countryNamesWithSystemDefault);
 	}
 
+	@Override
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+		Logger.i(this, "onSharedPreferenceChanged %s - key: %s", sharedPreferences, key);
+		if (getString(R.string.pref_key_default_content_country).equals(key)) {
+			String newCountry = sharedPreferences.getString(key, null);
+			NewPipeService.setCountry(newCountry);
+			EventBus.getInstance().notifyMainTabChanged(EventBus.SettingChange.CONTENT_COUNTRY);
+		}
+	}
 }
