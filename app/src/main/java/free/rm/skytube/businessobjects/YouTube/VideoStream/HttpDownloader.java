4fff1f694ed0e69a3cecbaf007816aeb103baa02
Zsombor Gegesy <gzsombor@gmail.com>
2021-04-04 21:17:04 +0200

    Proper close of the OutputStream in HttpDownloader

--- app/src/main/java/free/rm/skytube/businessobjects/YouTube/VideoStream/HttpDownloader.java
+++ app/src/main/java/free/rm/skytube/businessobjects/YouTube/VideoStream/HttpDownloader.java
@@ -79,7 +79,8 @@ public class HttpDownloader extends Downloader {
                 }
             }
 
-            try (OutputStream outputStream = sendOutput(request, connection)) {
+            sendOutput(request, connection);
+            try {
 
                 final String response = readResponse(connection);
 
@@ -107,16 +108,16 @@ public class HttpDownloader extends Downloader {
         }
     }
 
-    private OutputStream sendOutput(Request request, HttpsURLConnection connection) throws IOException {
+    private void sendOutput(Request request, HttpsURLConnection connection) throws IOException {
         final byte[] dataToSend = request.dataToSend();
         if (dataToSend != null && dataToSend.length > 0) {
             connection.setDoOutput(true);
             connection.setRequestProperty("Content-Length", String.valueOf(dataToSend.length));
-            OutputStream outputStream = connection.getOutputStream();
-            outputStream.write(dataToSend);
-            return outputStream;
+            try (OutputStream outputStream = connection.getOutputStream()) {
+                outputStream.write(dataToSend);
+                outputStream.flush();
+            }
         }
-        return null;
     }
 
     private String readResponse(HttpsURLConnection connection) throws IOException {
