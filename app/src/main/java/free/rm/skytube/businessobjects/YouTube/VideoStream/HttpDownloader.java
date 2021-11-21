4b67901ae137f7966cf9439781d980f8b80769e5
Ramon M <ramon.mifsud@yandex.com>
2021-11-21 17:37:20 +0100

    Updated HttpDownloader as it was getting an 400 error when downloading videos.  Updated libraries.

--- app/src/main/java/free/rm/skytube/businessobjects/YouTube/VideoStream/HttpDownloader.java
+++ app/src/main/java/free/rm/skytube/businessobjects/YouTube/VideoStream/HttpDownloader.java
@@ -1,6 +1,7 @@
 /*
  * SkyTube
- * Copyright (C) 2015  Ramon Mifsud
+ * Copyright (C) 2015  Ramon Mifsud -
+ * based on https://github.com/TeamNewPipe/NewPipe/blob/dev/app/src/main/java/org/schabi/newpipe/DownloaderImpl.java
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -21,103 +22,83 @@ import org.schabi.newpipe.extractor.downloader.Downloader;
 import org.schabi.newpipe.extractor.downloader.Request;
 import org.schabi.newpipe.extractor.downloader.Response;
 import org.schabi.newpipe.extractor.exceptions.ReCaptchaException;
-import org.schabi.newpipe.extractor.localization.Localization;
 
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.URL;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
-import javax.annotation.Nonnull;
-import javax.net.ssl.HttpsURLConnection;
+import androidx.annotation.NonNull;
+import okhttp3.OkHttpClient;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
 
 
 /**
  * Downloads HTTP content.
  */
-public class HttpDownloader extends Downloader {
+public final class HttpDownloader extends Downloader {
+	public static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0";
 
-	/** Mimic the Mozilla user agent */
-	private static final String USER_AGENT = "Mozilla/5.0";
+	private final Map<String, String> mCookies;
+	private final OkHttpClient client;
+
+	public HttpDownloader() {
+		this.client = new OkHttpClient.Builder()
+				.readTimeout(30, TimeUnit.SECONDS)
+				.build();
+		this.mCookies = new HashMap<>();
+	}
 
 	@Override
-	public Response execute(@Nonnull Request request) throws IOException, ReCaptchaException {
+	public Response execute(@NonNull final Request request) throws IOException, ReCaptchaException {
 		final String httpMethod = request.httpMethod();
 		final String url = request.url();
 		final Map<String, List<String>> headers = request.headers();
-		final Localization localization = request.localization();
+		final byte[] dataToSend = request.dataToSend();
 
-		final HttpsURLConnection connection = (HttpsURLConnection) new URL(url).openConnection();
+		RequestBody requestBody = null;
+		if (dataToSend != null) {
+			requestBody = RequestBody.create(null, dataToSend);
+		}
 
-		connection.setConnectTimeout(30 * 1000); // 30s
-		connection.setReadTimeout(30 * 1000); // 30s
-		connection.setRequestMethod(httpMethod);
+		final okhttp3.Request.Builder requestBuilder = new okhttp3.Request.Builder()
+				.method(httpMethod, requestBody).url(url)
+				.addHeader("User-Agent", USER_AGENT);
 
-		connection.setRequestProperty("User-Agent", USER_AGENT);
-		connection.setRequestProperty("Accept-Language", "en");
-
-		for (Map.Entry<String, List<String>> pair : headers.entrySet()) {
+		for (final Map.Entry<String, List<String>> pair : headers.entrySet()) {
 			final String headerName = pair.getKey();
 			final List<String> headerValueList = pair.getValue();
 
 			if (headerValueList.size() > 1) {
-				connection.setRequestProperty(headerName, null);
-				for (String headerValue : headerValueList) {
-					connection.addRequestProperty(headerName, headerValue);
+				requestBuilder.removeHeader(headerName);
+				for (final String headerValue : headerValueList) {
+					requestBuilder.addHeader(headerName, headerValue);
 				}
 			} else if (headerValueList.size() == 1) {
-				connection.setRequestProperty(headerName, headerValueList.get(0));
-			}
-		}
-
-		try (OutputStream outputStream = sendOutput(request, connection)) {
-
-			final String response = readResponse(connection);
-
-			final int responseCode = connection.getResponseCode();
-			final String responseMessage = connection.getResponseMessage();
-			final Map<String, List<String>> responseHeaders = connection.getHeaderFields();
-			final URL latestUrl = connection.getURL();
-			return new Response(responseCode, responseMessage, responseHeaders, response, latestUrl.toString());
-		} catch (Exception e) {
-			/*
-			 * HTTP 429 == Too Many Request
-			 * Receive from Youtube.com = ReCaptcha challenge request
-			 * See : https://github.com/rg3/youtube-dl/issues/5138
-			 */
-			if (connection.getResponseCode() == 429) {
-				throw new ReCaptchaException("reCaptcha Challenge requested", url);
+				requestBuilder.header(headerName, headerValueList.get(0));
 			}
 
-			throw new IOException(connection.getResponseCode() + " " + connection.getResponseMessage(), e);
 		}
-	}
 
-	private OutputStream sendOutput(Request request, HttpsURLConnection connection) throws IOException {
-		final byte[] dataToSend = request.dataToSend();
-		if (dataToSend != null && dataToSend.length > 0) {
-			connection.setDoOutput(true);
-			connection.setRequestProperty("Content-Length", dataToSend.length + "");
-			OutputStream outputStream = connection.getOutputStream();
-			outputStream.write(dataToSend);
-			return outputStream;
+		final okhttp3.Response response = client.newCall(requestBuilder.build()).execute();
+
+		if (response.code() == 429) {
+			response.close();
+
+			throw new ReCaptchaException("reCaptcha Challenge requested", url);
 		}
-		return null;
-	}
 
-	private String readResponse(HttpsURLConnection connection) throws IOException {
-		try (InputStreamReader input = new InputStreamReader(connection.getInputStream())) {
-			final StringBuilder response = new StringBuilder();
+		final ResponseBody body = response.body();
+		String responseBodyToReturn = null;
 
-			int readCount;
-			char[] buffer = new char[32 * 1024];
-			while ((readCount = input.read(buffer)) != -1) {
-				response.append(buffer, 0, readCount);
-			}
-			return response.toString();
+		if (body != null) {
+			responseBodyToReturn = body.string();
 		}
-	}
 
+		final String latestUrl = response.request().url().toString();
+		return new Response(response.code(), response.message(), response.headers().toMultimap(),
+				responseBodyToReturn, latestUrl);
+	}
 }
