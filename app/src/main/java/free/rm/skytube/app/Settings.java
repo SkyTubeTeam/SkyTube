d340967df96a439943eace9110c18b940fef9578
Zsombor Gegesy <gzsombor@gmail.com>
2022-06-28 22:22:30 +0200

    Hide the featured tab by default, as it is needs Youtube API keys

--- app/src/main/java/free/rm/skytube/app/Settings.java
+++ app/src/main/java/free/rm/skytube/app/Settings.java
@@ -38,6 +38,7 @@ import free.rm.skytube.app.enums.Policy;
 import free.rm.skytube.businessobjects.Logger;
 import free.rm.skytube.businessobjects.YouTube.VideoStream.VideoQuality;
 import free.rm.skytube.businessobjects.YouTube.VideoStream.VideoResolution;
+import free.rm.skytube.gui.fragments.MainFragment;
 import free.rm.skytube.gui.fragments.SubscriptionsFeedFragment;
 
 /**
@@ -67,6 +68,10 @@ public class Settings {
         setDefault(sharedPreferences, R.string.pref_key_video_quality_on_mobile, VideoQuality.LEAST_BANDWITH.name());
         setDefault(sharedPreferences, R.string.pref_key_use_newer_formats, Build.VERSION.SDK_INT > 16);
         setDefault(sharedPreferences, R.string.pref_key_playback_speed, "1.0");
+        Set<String> defaultTabs = new HashSet<>();
+        defaultTabs.add(MainFragment.FEATURED_VIDEOS_FRAGMENT);
+        setDefault(sharedPreferences, R.string.pref_key_hide_tabs, defaultTabs);
+        setDefault(sharedPreferences, R.string.pref_key_default_tab_name, MainFragment.MOST_POPULAR_VIDEOS_FRAGMENT);
     }
 
     private void migrate(SharedPreferences sharedPreferences, String oldKey, @StringRes int newKey) {
@@ -90,6 +95,8 @@ public class Settings {
                 editor.putString(keyStr, (String) defaultValue);
             } else if (defaultValue instanceof Boolean) {
                 editor.putBoolean(keyStr, (Boolean) defaultValue);
+            } else if (defaultValue instanceof Set) {
+                editor.putStringSet(keyStr, (Set<String>) defaultValue);
             } else {
                 throw new IllegalArgumentException("Default value is " + defaultValue + " for " + keyStr);
             }
