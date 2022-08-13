fd96dfc7fbb987abf22ba74843f90f0f636cbbc2
Zsombor Gegesy <gzsombor@gmail.com>
2022-08-14 01:27:51 +0200

    Fix test case, more debug msgs

--- app/src/main/java/free/rm/skytube/app/StreamSelectionPolicy.java
+++ app/src/main/java/free/rm/skytube/app/StreamSelectionPolicy.java
@@ -58,7 +58,7 @@ public class StreamSelectionPolicy {
     public StreamSelection select(StreamInfo streamInfo) {
         VideoStreamWithResolution videoStreamWithResolution = pickVideo(streamInfo);
         if (videoStreamWithResolution != null) {
-            if (videoStreamWithResolution.videoStream.isVideoOnly) {
+            if (videoStreamWithResolution.videoStream.isVideoOnly()) {
                 AudioStream audioStream = pickAudio(streamInfo);
                 if (audioStream != null) {
                     return new StreamSelection(videoStreamWithResolution.videoStream, videoStreamWithResolution.resolution, audioStream);
@@ -119,7 +119,7 @@ public class StreamSelectionPolicy {
     }
 
     private static String toHumanReadable(AudioStream as) {
-        return as != null ? "AudioStream(" + as.getAverageBitrate() + ", " + as.getFormat() + ", codec=" + as.getCodec() + ", q=" + as.getQuality() + ", isUrl=" + as.isUrl() + ")" : "NULL";
+        return as != null ? "AudioStream(" + as.getAverageBitrate() + ", " + as.getFormat() + ", codec=" + as.getCodec() + ", q=" + as.getQuality() + ", isUrl=" + as.isUrl() + ",delivery=" + as.getDeliveryMethod() + ")" : "NULL";
     }
 
     private boolean isBetter(AudioStream best, AudioStream other) {
@@ -226,7 +226,13 @@ public class StreamSelectionPolicy {
         }
 
         private String toHumanReadable() {
-            return "VideoStream(" + resolution.name() + ", format=" + videoStream.getFormat() + ", codec=" + videoStream.getCodec() + ", quality=" + videoStream.getQuality() + ",videoOnly=" + videoStream.isVideoOnly() + ",isUrl=" + videoStream.isUrl() + ")";
+            return "VideoStream(" + resolution.name() +
+                    ", format=" + videoStream.getFormat() +
+                    ", codec=" + videoStream.getCodec() +
+                    ", quality=" + videoStream.getQuality() +
+                    ",videoOnly=" + videoStream.isVideoOnly() +
+                    ",isUrl=" + videoStream.isUrl() +
+                    ",delivery=" + videoStream.getDeliveryMethod() + ")";
         }
 
         private static String toHumanReadable(VideoStreamWithResolution v) {
